5e1dce52a55ca3f6bc5a3bc1534258ff03bbf1ee
gregemax <gregemax700@gmail.com>
2025-06-25 10:50:19 +0100

    done

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const crypto = require("crypto")
-const keccak = require("keccak")
-
-
+const keccak = require("keccak");
+const { exit } = require("process");
+ var prompt = require('prompt')
 const hash = "123"
+prompt.start();
 
-const type = "sha256"
 
-const test = ()=>{
- if (type=="sha256") {
-    console.log(crypto.createHash(type).update(hash).digest("hex"))
- }
- else{
-const keccak25=keccak("keccak256").update(hash).digest("hex")
-console.log(keccak25) 
- }
+
+
+const test = (cryptoo,keccakk)=>{
+   if (cryptoo) {
+      console.log(crypto.createHash("sha256").update(cryptoo).digest("hex"))
+   }
+   if(keccakk){
+      const keccak25=keccak("keccak256").update(keccakk).digest("hex")
+      console.log(keccak25) 
+   }
+   
 }
 
-test()
+
+
+prompt.get(['sha256', 'keccak256'], function (err, result) {
+   console.log('Command-line input received:');
+   console.log('  username: ' + result.sha256);
+   console.log('  email: ' + result.keccak256);
+   test(result.sha256,result.keccak256)
+   exit()
+  });
